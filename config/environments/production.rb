3e64bcacfe34981b758ab43b8ee0ecf8bc03ca86
Mark Oleson <fusion2004@gmail.com>
2013-02-07 14:27:01 -0500

    changed mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Tomgang::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'tomgang.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.tomgang.mn' }
 end
