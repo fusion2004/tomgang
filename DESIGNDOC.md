4719bedd246231c35f37a5409569eb2a63726444
Mark Oleson <fusion2004@gmail.com>
2013-02-07 12:27:03 -0500

    rails init

---
+++ DESIGNDOC.md
@@ -0,0 +1,8 @@
+## Basic game
+
+Make an account, you level up every log10(x^x) + x^2 seconds, where x is the integer level you are trying to reach.
+Players start at level 1.
+
+  As an example, level 2 is reached in 5.4 seconds, level 3 is reached 12.3 seconds, level 4 in 21.5 seconds.
+
+You have to be on the website in order to level up.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tomgang::Application.config.secret_token = '8af310c580286704e2e06a4a62f949da6bc6c9a8931f8b7d530a6713744bda57a4f8697d345b8ae09947037d77674a74180b303143baac58ad3650fee38ecf8c'
