34ea59947022c204312bb9128875bf28b3a39e59
Mark Oleson <fusion2004@gmail.com>
2013-02-07 21:28:45 -0500

    level progress bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
   def check_level
     if user_signed_in?
       current_user.check_level
+
+      total_secs = (current_user.next_level_up.to_i - current_user.last_level_up.to_i).to_f
+      current_secs = (current_user.next_level_up.to_i - Time.now.to_i).to_f
+      @level_percent = (current_secs / total_secs) * 100
     end
   end
 end
