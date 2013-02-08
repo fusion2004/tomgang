59fe5acc82dfefd3bab7283960faf67818bad6b3
Mark Oleson <fusion2004@gmail.com>
2013-02-08 12:20:57 -0500

    display hp and mana

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,9 @@ class ApplicationController < ActionController::Base
       @current_secs = (current_user.next_level_up.to_i - Time.now.to_i)
       inv_current_secs = total_secs - @current_secs
       @level_percent = (inv_current_secs.to_f / total_secs.to_f) * 100
+
+      @hp_percent = (current_user.current_hp.to_f / current_user.max_hp.to_f) * 100
+      @mana_percent = (current_user.current_mana.to_f / current_user.max_mana.to_f) * 100
     end
   end
 end
