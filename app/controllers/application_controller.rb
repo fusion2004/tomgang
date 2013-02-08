a18050d62f821a21797fc403d7597ed16eb623d0
Mark Oleson <fusion2004@gmail.com>
2013-02-07 20:35:26 -0500

    leveling and login field stuffs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :check_level
+
+  private
+
+  def check_level
+    if user_signed_in?
+      current_user.check_level
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,39 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: { :case_sensitive => false }
 
+  before_create :init_level
+
+  def init_level
+    self.level = 1
+  end
+
+  def init_leveling!
+    self.last_level_up = Time.now
+    save
+
+    calc_next_level_up!
+  end
+
+  def check_level
+    if self.next_level_up == nil
+      init_leveling!
+    elsif self.next_level_up <= Time.now
+      level_up!
+    end
+  end
+
+  def level_up!
+    self.level = self.level + 1
+    save
+
+    calc_next_level_up!
+  end  
+
+  def calc_next_level_up!
+    next_level = self.level + 1
+    secs = Math.log(next_level ** next_level, Math::E) + next_level ** 2
+
+    self.next_level_up = self.last_level_up + secs
+    save
+  end
 end
